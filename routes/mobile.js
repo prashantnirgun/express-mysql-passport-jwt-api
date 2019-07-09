7134c7f532db85aadf234f5d10e9e86f42a923b3
Prashant <prashant.nirgun@gmail.com>
2019-07-09 11:37:09 +0530

    customer query updated

--- routes/mobile.js
+++ routes/mobile.js
@@ -6,7 +6,7 @@ const pool = require("../database");
 module.exports = app => {
   app.get("/mobile/customer", (req, res) => {
     let sql =
-      "select id, customer_name1, case market when 'v' then 'Veg' when 'f' then 'Fruit' when 'O' then 'Onion' else 'Retail' end as market, concat(wing,' ', gala_no) as gala_no from tss_bpp_customer";
+      "select id, customer_name1 as name, case market when 'v' then 'Veg' when 'f' then 'Fruit' when 'O' then 'Onion' else 'Retail' end as market, concat(wing,' ', gala_no) as gala_no from tss_bpp_customer";
     pool.query(sql).then(result => {
       res.send(result);
     });
