51ee4ff49b2643f57b01673bbd75648168153b4b
Prashant <prashant.nirgun@gmail.com>
2019-07-25 17:58:16 +0530

    rename customer to customer-company-list new GET customer-company/:customer_id/:co_id endpoint created also added deleted_by_user_id to all endpoints

--- routes/mobile.js
+++ routes/mobile.js
@@ -1,31 +1,53 @@
-const { parseResultSet, readBody } = require("../helpers");
+//const { parseResultSet, readBody } = require("../helpers");
 //const isAuthenticated = require("../../policies/isAutheticated");
 //const model = require("../models/index");
 const pool = require("../database");
 
 module.exports = app => {
   app.get("/mobile/customer", (req, res) => {
-    let sql =
-      "select id, customer_name1 as name, case market when 'v' then 'Veg' when 'f' then 'Fruit' when 'O' then 'Onion' else 'Retail' end as market, concat(wing,' ', gala_no) as gala_no from tss_bpp_customer where deleted_by_user_id = 0";
+    let sql = `select id, customer_name1 as name, 
+      case market when 'v' then 'Veg' when 'f' then 'Fruit' when 'O' then 'Onion' else 'Retail' end as market, 
+      concat(wing,' ', gala_no) as gala_no 
+      from tss_bpp_customer 
+      where deleted_by_user_id = 0`;
+
     pool.query(sql).then(result => {
       res.send(result);
     });
   });
 
   app.get("/mobile/customer/:id", (req, res) => {
-    let sql =
-      "select customer_name1, customer_name2, owner_name, contact_person, contact_no1, contact_no2, email, no_of_company, market, wing, gala_no, sms, backup, mobile from tss_bpp_customer where id = " +
-      req.params.id;
+    let sql = `select customer_name1, customer_name2, owner_name, contact_person, contact_no1, 
+      contact_no2, email, no_of_company, market, wing, gala_no, sms, backup, mobile 
+      from tss_bpp_customer where id = ${
+        req.params.id
+      } AND deleted_by_user_id = 0`;
 
     pool.query(sql).then(result => {
       res.send(result[0]);
     });
   });
 
-  app.get("/mobile/company/:customer_id", (req, res) => {
-    let sql =
-      "select co_id, company_name, starting_year, current_year, company_status from tss_bpp_company where customer_id = " +
-      req.params.customer_id;
+  app.get("/mobile/customer-company-list/:customer_id", (req, res) => {
+    let sql = `select co_id, company_name, starting_year, current_year, company_status 
+      from tss_bpp_company 
+      where customer_id = ${req.params.customer_id} AND deleted_by_user_id = 0`;
+
+    pool.query(sql).then(result => {
+      res.send(result);
+    });
+  });
+
+  9892173659;
+  9867750798;
+  9224171131;
+  app.get("/mobile/customer-company/:customer_id/:co_id", (req, res) => {
+    let sql = `select id, branch_id, co_id, current_year, 
+      company_name, starting_year, patti_dw, patti_format_dw, memo_dw, memo_format_dw,
+      sale_bill, sale_challan, font, language, company_status 
+      from tss_bpp_company 
+      where customer_id = ${req.params.customer_id} AND 
+      co_id = ${req.params.co_id} AND deleted_by_user_id = 0`;
 
     pool.query(sql).then(result => {
       res.send(result);
