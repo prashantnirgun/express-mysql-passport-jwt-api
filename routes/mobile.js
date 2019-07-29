b2c458be5a65e9f18e145827603606e446c8ec07
Prashant <prashant.nirgun@gmail.com>
2019-07-29 17:41:49 +0530

    POST customer added

--- routes/mobile.js
+++ routes/mobile.js
@@ -50,6 +50,52 @@ module.exports = app => {
     });
   });
 
+  app.post("/mobile/company", (req, res) => {
+    let {
+      id,
+      co_id,
+      branch_id,
+      current_year,
+      company_name,
+      starting_year,
+      patti_dw,
+      patti_format_dw,
+      memo_dw,
+      memo_format_dw,
+      sale_bill,
+      sale_challan,
+      font,
+      language,
+      company_status
+    } = req.body;
+    let sql = "";
+
+    if (parseInt(id) === 0) {
+      sql = `INSERT tss_bpp_company 
+      (id, co_id, branch_id, current_year, company_name, starting_year, 
+      patti_dw, patti_format_dw, memo_dw, memo_format_dw, 
+      sale_bill, sale_challan, font, language, company_status,
+      created_at, created_by_user_id, updated_at, updated_by_user_at, deleted_at, deleted_by_user_id)
+      VALUES(${id}, ${co_id}, 1, ${current_year}, '${company_name}', '${starting_year}',
+      '${patti_dw}', '${patti_format_dw}', '${memo_dw}', '${memo_format_dw}',
+      '${sale_bill}', '${sale_challan}', '${font}', '${language}', '${company_status}),
+      1,now(),null,null)`;
+    } else {
+      sql = `UPDATE tss_bpp_company 
+      SET co_id = ${co_id}, current_year = ${current_year}, company_name = '${company_name}', 
+      starting_year = '${starting_year}', patti_dw = '${patti_dw}', patti_format_dw = '${patti_format_dw}', 
+      memo_dw = '${memo_dw}', memo_format_dw = '${memo_format_dw}', sale_bill = '${sale_bill}', 
+      sale_challan = '${sale_challan}', font = '${font}', language = '${language}', 
+      company_status = '${company_status}', updated_at = now()
+      where id = ${id} `;
+    }
+    console.log(sql);
+
+    pool.query(sql).then(result => {
+      res.send(result);
+    });
+  });
+
   app.post("/mobile/customerSearch/", (req, res) => {
     const { customerName, galaNo, apmc, wing } = req.body;
     let nameWhere = "";
